f24c369d5d083655980e4e5eaf21661e46197e4c
IceMetalPunk <IceMetalPunk@gmail.com>
2023-02-15 01:52:50 -0500

    Basic shared memory render thread

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,35 @@ if (globalThis.crossOriginIsolated) {
     const myWorker = new Worker('/js/worker.js');
     const buffer = new SharedArrayBuffer(16);
     const uint8 = new Uint8Array(buffer);
-    myWorker.postMessage({ buffer, canvas }, [canvas]);
+    const imageMaps = new Map();
+    myWorker.addEventListener('message', message => {
+        if (message.data.type === 'loadedImage') {
+            imageMaps.set(message.data.name, message.data.id);
+            console.log(`${message.data.name} = #${message.data.id}`, imageMaps);
+        }
+    });
+    myWorker.postMessage({ type: 'init', buffer, canvas }, [canvas]);
     setInterval(() => {
         Atomics.add(uint8, 0, 1);
     }, 1000);
+    setTimeout(() => {
+        const img = new Image();
+        img.addEventListener('load', async () => {
+            const bmp = await createImageBitmap(img);
+            myWorker.postMessage({ type: 'loadImage', name: 'sprHalo', bmp}, [bmp]);
+            delete img;
+        }, { once: true });
+        img.src = '/img/halo_test.png';
+    }, 5000);
+    setTimeout(() => {
+        const img = new Image();
+        img.addEventListener('load', async () => {
+            const bmp = await createImageBitmap(img);
+            myWorker.postMessage({ type: 'loadImage', name: 'sprHalo2', bmp}, [bmp]);
+            delete img;
+        }, { once: true });
+        img.src = '/img/halo_test.png';
+    }, 10000);
 } else {
     console.log('NOT SECURE!');
 }
--- public/js/worker.js
+++ public/js/worker.js
@@ -1,4 +1,5 @@
-globalThis.addEventListener('message', message => {
+const images = [];
+const start = message => {
     const { buffer, canvas} = message.data;
     const ctx = canvas.getContext('2d');
     const uint8 = new Uint8Array(buffer);
@@ -9,7 +10,23 @@ globalThis.addEventListener('message', message => {
         ctx.fillStyle = '#000000';
         ctx.textBaseline = 'top';
         ctx.fillText(txt, 2, 2, canvas.width - 4);
+        images.forEach((img, i) => ctx.drawImage(img, 10 + 25 * i, 10));
         requestAnimationFrame(draw);
     }
     requestAnimationFrame(draw);
+}
+globalThis.addEventListener('message', message => {
+    switch (message.data.type) {
+        case 'init':
+            start(message);
+            break;
+        case 'loadImage':
+            images.push(message.data.bmp);
+            globalThis.postMessage({
+                type: 'loadedImage',
+                name: message.data.name,
+                id: images.length - 1
+            });
+            break;
+    }
 })
